03cd5cdda842b631041bfdb6643c63c98f193549
Wilfredo Crackbones <wilfredo@student.21-school.ru>
2023-02-22 16:14:34 +0000

    wilfredo

--- src/part1.sql
+++ src/part1.sql
@@ -3,7 +3,6 @@ create table peers (
     birthday date not null
 );
 
-
 create table friends (
     id bigint primary key,
     peer1 varchar not null,
@@ -29,9 +28,51 @@ create table transferred_points (
     constraint fk_friends_checked_peer foreign key (checked_peer) references peers(nickname)
 );
 
+create table time_tracking (
+    id bigint primary key,
+    peer varchar not null,
+    date_ date not null,
+    time_ time not null,
+    state varchar not null,
+    constraint check_time_tracking_state check (state in ('In', 'Out')),
+    constraint fk_time_tracking_peer foreign key (peer) references peers(nickname)
+);
+
+create table checks (
+    id bigint primary key,
+    peer varchar not null,
+    task varchar not null,
+    date_ date not null,
+    constraint fk_checks_peer foreign key (peer) references peers(nickname),
+    constraint fk_checks_task foreign key (task) references tasks(title)
+);
+
 create table p2p (
-    
-)
+    id bigint primary key,
+    check_ bigint not null,
+    checking_peer varchar not null,
+    state varchar not null,
+    time_ timestamp default current_timestamp,
+    constraint check_p2p_state check (state in ('Start', 'Success', 'Failure')),
+    constraint fk_p2p_check_ foreign key (check_) references checks(id),
+    constraint fk_p2p_checking_peer foreign key (checking_peer) references peers(nickname)
+);
+
+create table verter (
+    id bigint primary key,
+    check_ bigint not null,
+    state varchar not null,
+    time_ timestamp default current_timestamp,
+    constraint check_p2p_state check (state in ('Start', 'Success', 'Failure')),
+    constraint fk_verter_check_ foreign key (check_) references checks(id)
+);
+
+create table xp (
+    id bigint primary key,
+    check_ bigint not null,
+    xp_amount bigint not null,
+    constraint fk_xp_check_ foreign key (check_) references checks(id)
+);
 
 create table tasks (
     title varchar primary key,
