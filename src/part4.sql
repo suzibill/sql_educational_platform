d1c88ff6b8961c977dc7c48e58db117deb565d95
Wilfredo Crackbones <wilfredo@student.21-school.ru>
2023-03-13 17:47:32 +0000

    styled

--- src/part4.sql
+++ src/part4.sql
@@ -1,220 +1,222 @@
-create table if not exists table_1 (
-    id bigint primary key,
-    string varchar not null
+CREATE TABLE IF NOT EXISTS table_1
+(
+    id     bigint PRIMARY KEY,
+    string varchar NOT NULL
 );
 
-create table if not exists table_2 (
-    id bigint primary key,
-    string varchar not null
+CREATE TABLE IF NOT EXISTS table_2
+(
+    id     bigint PRIMARY KEY,
+    string varchar NOT NULL
 );
 
-create table if not exists table_3 (
-    id bigint primary key,
-    string varchar not null
+CREATE TABLE IF NOT EXISTS table_3
+(
+    id     bigint PRIMARY KEY,
+    string varchar NOT NULL
 );
 
-create table if not exists test_1 (
-    id bigint primary key,
-    string varchar not null
+CREATE TABLE IF NOT EXISTS test_1
+(
+    id     bigint PRIMARY KEY,
+    string varchar NOT NULL
 );
 
-create table if not exists test_2 (
-    id bigint primary key,
-    string varchar not null
+CREATE TABLE IF NOT EXISTS test_2
+(
+    id     bigint PRIMARY KEY,
+    string varchar NOT NULL
 );
 
-create table if not exists test_3 (
-    id bigint primary key,
-    string varchar not null
+CREATE TABLE IF NOT EXISTS test_3
+(
+    id     bigint PRIMARY KEY,
+    string varchar NOT NULL
 );
 
 
-create or replace function fnc_table_ins() returns trigger as
-    $$
-    begin
-        return new;
-    end;
-    $$
-language plpgsql;
-
-create or replace function fnc_table_upd() returns trigger as
-    $$
-    begin
-        return new;
-    end;
-    $$
-language plpgsql;
-
-create or replace function fnc_test_ins() returns trigger as
-    $$
-    begin
-        return new;
-    end;
-    $$
-language plpgsql;
-
-create or replace function fnc_test_upd() returns trigger as
-    $$
-    begin
-        return new;
-    end;
-    $$
-language plpgsql;
-
-create trigger trg_test_1
-    after insert
-    on table_1
-    for each row
-execute function fnc_table_ins();
-
-create trigger trg_test_2
-    after update
-    on table_1
-    for each row
-execute function fnc_table_upd();
-
-create trigger trg_test_3
-    before insert
-    on table_1
-    for each row
-execute function fnc_table_ins();
-
-create trigger trg_test_4
-    after insert
-    on test_1
-    for each row
-execute function fnc_test_ins();
-
-create trigger trg_test_5
-    after update
-    on test_1
-    for each row
-execute function fnc_test_upd();
-
-create trigger trg_test_6
-    before insert
-    on test_1
-    for each row
-execute function fnc_test_ins();
-
-create or replace function fnc_table_test_1(in n integer, out res integer) as
+CREATE OR REPLACE FUNCTION fnc_table_ins() RETURNS TRIGGER AS
 $$
-    begin
-        select 6 into n;
-    end;
+BEGIN
+    RETURN new;
+END;
 $$
-language plpgsql;
+    LANGUAGE plpgsql;
 
-create or replace function fnc_table_test_2(in n integer,in c integer, out res integer) as
+CREATE OR REPLACE FUNCTION fnc_table_upd() RETURNS TRIGGER AS
 $$
-    begin
-        select 6 into n;
-    end;
+BEGIN
+    RETURN new;
+END;
 $$
-language plpgsql;
+    LANGUAGE plpgsql;
 
-create or replace function fnc_table_test_1(in n integer) returns integer as
+CREATE OR REPLACE FUNCTION fnc_test_ins() RETURNS TRIGGER AS
 $$
-    begin
-        select 6 into n;
-        return 10;
-    end;
+BEGIN
+    RETURN new;
+END;
 $$
-language plpgsql;
+    LANGUAGE plpgsql;
 
-create or replace procedure prc_delete_table() as
+CREATE OR REPLACE FUNCTION fnc_test_upd() RETURNS TRIGGER AS
 $$
-declare
+BEGIN
+    RETURN new;
+END;
+$$
+    LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_test_1
+    AFTER INSERT
+    ON table_1
+    FOR EACH ROW
+EXECUTE FUNCTION fnc_table_ins();
+
+CREATE TRIGGER trg_test_2
+    AFTER UPDATE
+    ON table_1
+    FOR EACH ROW
+EXECUTE FUNCTION fnc_table_upd();
+
+CREATE TRIGGER trg_test_3
+    BEFORE INSERT
+    ON table_1
+    FOR EACH ROW
+EXECUTE FUNCTION fnc_table_ins();
+
+CREATE TRIGGER trg_test_4
+    AFTER INSERT
+    ON test_1
+    FOR EACH ROW
+EXECUTE FUNCTION fnc_test_ins();
+
+CREATE TRIGGER trg_test_5
+    AFTER UPDATE
+    ON test_1
+    FOR EACH ROW
+EXECUTE FUNCTION fnc_test_upd();
+
+CREATE TRIGGER trg_test_6
+    BEFORE INSERT
+    ON test_1
+    FOR EACH ROW
+EXECUTE FUNCTION fnc_test_ins();
+
+CREATE OR REPLACE FUNCTION fnc_table_test_1(IN n integer, OUT res integer) AS
+$$
+BEGIN
+    SELECT 6 INTO n;
+END;
+$$
+    LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION fnc_table_test_2(IN n integer, IN c integer, OUT res integer) AS
+$$
+BEGIN
+    SELECT 6 INTO n;
+END;
+$$
+    LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION fnc_table_test_1(IN n integer) RETURNS integer AS
+$$
+BEGIN
+    SELECT 6 INTO n;
+    RETURN 10;
+END;
+$$
+    LANGUAGE plpgsql;
+
+CREATE OR REPLACE PROCEDURE prc_delete_table() AS
+$$
+DECLARE
     table_name_del varchar ='test';
+    row            record;
+BEGIN
+    FOR row IN
+        (SELECT table_name
+         FROM information_schema.tables
+         WHERE table_schema = 'public'
+           AND table_name LIKE format('%s%%', table_name_del))
+        LOOP
+            EXECUTE 'delete table ' || row.table_name || ' cascade';
+        END LOOP;
+END;
+$$
+    LANGUAGE plpgsql;
+
+CALL prc_delete_table();
+
+CREATE OR REPLACE PROCEDURE prc_part4_task2(
+    INOUT results integer,
+    IN ref REFCURSOR
+) AS
+$$
+BEGIN
+    OPEN ref FOR
+        SELECT pc.proname,
+               pg_catalog.pg_get_function_identity_arguments(pc.oid) AS pg_arg
+        FROM pg_catalog.pg_namespace pn
+                 JOIN pg_catalog.pg_proc pc ON pc.pronamespace = pn.oid
+        WHERE pc.prokind = 'f'
+          AND pg_catalog.pg_get_function_identity_arguments(pc.oid) IS NOT NULL
+          AND pn.nspname = 'public';
+    results := (SELECT COUNT(pc.proname)
+                FROM pg_catalog.pg_namespace pn
+                         JOIN pg_catalog.pg_proc pc ON pc.pronamespace = pn.oid
+                WHERE pc.prokind = 'f'
+                  AND pg_catalog.pg_get_function_identity_arguments(pc.oid) IS NOT NULL
+                  AND pn.nspname = 'public');
+END;
+$$ LANGUAGE plpgsql;
+
+BEGIN;
+CALL prc_part4_task2(0, 'ref');
+FETCH ALL IN "ref";
+END;
+
+SELECT * FROM information_schema.triggers;
+SELECT * FROM pg_proc WHERE prosrc LIKE '%BEGIN%';
+
+CREATE OR REPLACE PROCEDURE prc_part4_task3(
+    INOUT results integer
+) AS
+$$
+DECLARE
     row record;
-begin
-    for row in
-        (select table_name
-        from information_schema.tables
-        where table_schema = 'public'
-            and table_name like format('%s%%',table_name_del))
-        loop
-        execute  'delete table ' || row.table_name || ' cascade';
-        end loop;
-end;
-$$
-language plpgsql;
+BEGIN
+    results = (SELECT COUNT(*)
+               FROM information_schema.triggers);
+    FOR row IN (SELECT trigger_name,
+                       event_object_table
+                FROM information_schema.triggers)
+        LOOP
+            EXECUTE format('drop trigger %s ON %s cascade', row.trigger_name, row.event_object_table);
+        END LOOP;
+END;
+$$ LANGUAGE plpgsql;
 
-call prc_delete_table();
+CALL prc_part4_task3(NULL);
 
-create or replace procedure  prc_part4_task2(
-    inout results integer,
-    in ref refcursor
+CREATE OR REPLACE PROCEDURE prc_part4_task4(
+    IN param varchar,
+    IN ref REFCURSOR
 ) as
 $$
-begin
-open ref for
-select pc.proname,
-    pg_catalog.pg_get_function_identity_arguments(pc.oid) as pg_arg
-    from pg_catalog.pg_namespace pn
-    join pg_catalog.pg_proc pc on pc.pronamespace = pn.oid
-    where pc.prokind = 'f'
-    and
-    pg_catalog.pg_get_function_identity_arguments(pc.oid) is not null
-        and pn.nspname = 'public';
-results := (
-    select
-        count(pc.proname)
-    from pg_catalog.pg_namespace pn
-    join pg_catalog.pg_proc pc on pc.pronamespace = pn.oid
-    where pc.prokind = 'f'
-    and
-    pg_catalog.pg_get_function_identity_arguments(pc.oid) is not null
-        and pn.nspname = 'public');
-end;
-$$ language  plpgsql;
+BEGIN
+    OPEN ref FOR
+        SELECT proname, prosrc
+        FROM pg_proc
+                 JOIN pg_namespace pn ON pg_proc.pronamespace = pn.oid
+            AND nspname = 'public'
+        WHERE prosrc LIKE '%' || param || '%';
+END;
+$$ LANGUAGE plpgsql;
 
-begin ;
-call prc_part4_task2(0, 'ref');
-fetch all in "ref";
-end;
+BEGIN;
+CALL prc_part4_task4('s', 'ref');
+FETCH ALL IN "ref";
+END;
 
-select * from information_schema.triggers;
-select * from pg_proc where prosrc like '%BEGIN%';
-
-create or replace procedure prc_part4_task3(
-inout results integer
-) as
-$$
-declare row record;
-begin
-    results = (select
-                   count(*)
-                from information_schema.triggers);
-    for row in (select
-                    trigger_name, event_object_table
-                from information_schema.triggers)
-    loop
-        execute format('drop trigger %s on %s cascade', row.trigger_name, row.event_object_table);
-    end loop;
-end;
-$$ language plpgsql;
-
-call prc_part4_task3(null);
-
-create or replace procedure  prc_part4_task4 (
-    in param varchar,
-    in ref refcursor
-) as
-$$
-begin
-    open ref for
-        select proname, prosrc
-        from pg_proc
-        join pg_namespace pn on pg_proc.pronamespace = pn.oid
-            and nspname = 'public'
-        where prosrc like '%' || param || '%';
-end;
-$$ language plpgsql;
-
-begin;
-call prc_part4_task4('s', 'ref');
-fetch all in "ref";
-end;
-
-call prc_part4_task4('begin', 'ref');
+CALL prc_part4_task4('BEGIN', 'ref');
