b83794ce3854171b1cce4af3097f9cda1de819a6
Wilfredo Crackbones <wilfredo@student.21-school.ru>
2023-03-13 16:45:44 +0000

    styled

--- src/part2.sql
+++ src/part2.sql
@@ -1,124 +1,108 @@
 /* PART 1 */
-create or replace procedure
+CREATE OR REPLACE PROCEDURE
     insert_p2p(checked_peer varchar, checking_peer__ varchar, task_ varchar, status state_type)
-    -- checked_peer peer being checked
-    -- checking_peer__ peer who is checking
-as
+AS
 $$
-begin
-    if status = 'Start' then
-        insert into checks(id, peer, task, date_) values ((select max(id) + 1 from checks), checked_peer, task_, current_date);
-    end if;
-    insert into p2p(id, check_, checking_peer, state_, time_)
-    values ((select max(id) + 1 from p2p),
-            (select max(id) from checks where peer = checked_peer and task_ = task), checking_peer__, status,
+BEGIN
+    IF status = 'Start' THEN
+        INSERT INTO checks(id, peer, task, date_)
+        VALUES ((SELECT MAX(id) + 1 FROM checks), checked_peer, task_, current_date);
+    END IF;
+    INSERT INTO p2p(id, check_, checking_peer, state_, time_)
+    VALUES ((SELECT MAX(id) + 1 FROM p2p),
+            (SELECT MAX(id) FROM checks WHERE peer = checked_peer AND task_ = task), checking_peer__, status,
             current_time);
-end;
-$$ language 'plpgsql';
+END;
+$$ LANGUAGE 'plpgsql';
 
-call insert_p2p('Aboba', 'Amogus', 'C3_s21_string', 'Failure');
--- TODO переписать инсерт в p2p чтобы он брал check id не из checks, а из p2p (последняя проверка этого пира)
--- Хотя тут есть вопросы к реализации
--- insert into checks(id, peer, task, date_)
--- values ((select max(id) + 1 from checks), 'Aboba', 'C2_SimpleBashUtils', current_date);
--- insert into p2p(id, check_, checking_peer, state_, time_)
--- values ((select max(id) + 1 from p2p),
---         (select max(id) from checks where peer = 'Aboba' and 'C2_SimpleBashUtils' = task), 'Amogus', 'Failure',
---         current_time);
--- select *
--- from p2p;
--- select *
--- from checks;
+CALL insert_p2p('Aboba', 'Amogus', 'C3_s21_string', 'Failure');
 
 /* PART 2 */
-create or replace procedure
+CREATE OR REPLACE PROCEDURE
     insert_verter(nick varchar, task__ varchar, status state_type, time__ time)
-as
+AS
 $$
-begin
-    insert into verter(id, check_, state_, time_)
-    values ((select max(id) + 1 from verter),
-            (select check_
-             from p2p
-                      join checks c on p2p.check_ = c.id
-             where peer = nick
-               and task = task__
-               and state_ = 'Success'
-             order by check_ desc
-             limit 1),
+BEGIN
+    INSERT INTO verter(id, check_, state_, time_)
+    VALUES ((SELECT MAX(id) + 1 FROM verter),
+            (SELECT check_
+             FROM p2p
+                      JOIN checks c ON p2p.check_ = c.id
+             WHERE peer = nick
+               AND task = task__
+               AND state_ = 'Success'
+             ORDER BY check_ DESC
+             LIMIT 1),
             status,
             time__);
-end;
-$$ language 'plpgsql';
+END;
+$$ LANGUAGE 'plpgsql';
 
- call insert_verter('Aboba', 'C3_s21_string', 'Success', '22:00:01');
---
--- select check_
--- from p2p
---          join checks c on p2p.check_ = c.id
--- where peer = 'Aboba'
---   and task = 'C3_s21_string'
---   and state_ = 'Success'
--- order by check_ desc
--- limit 1;
+CALL insert_verter('Aboba', 'C3_s21_string', 'Success', '22:00:01');
 
 /* PART 3 */
-create or replace function fnc_trg_transferred_points()
-    returns trigger as
+CREATE OR REPLACE FUNCTION fnc_trg_transferred_points()
+    RETURNS TRIGGER AS
 $trg_trans$
-begin
-    update transferred_points
-    set point_amount = point_amount + 1
-    where new.checking_peer = new.checking_peer
-      and checked_peer in (select peer from checks where checks.id = new.check_);
-    return new;
-end;
-$trg_trans$ language 'plpgsql';
+BEGIN
+    UPDATE transferred_points
+    SET point_amount = point_amount + 1
+    WHERE new.checking_peer = new.checking_peer
+      AND checked_peer IN (SELECT peer FROM checks WHERE checks.id = new.check_);
+    RETURN new;
+END;
+$trg_trans$ LANGUAGE 'plpgsql';
 
-create or replace trigger trg_transferred_points
-after insert on p2p
-    for each row
-    when (new.state_ = 'Start')
-execute procedure fnc_trg_transferred_points();
-call insert_p2p('Aboba', 'Sus', 'C3_s21_string', 'Start');
+CREATE OR REPLACE TRIGGER trg_transferred_points
+    AFTER INSERT
+    ON p2p
+    FOR EACH ROW
+    WHEN (new.state_ = 'Start')
+EXECUTE PROCEDURE fnc_trg_transferred_points();
 
-select * from transferred_points
-where checking_peer = 'Sus' and checked_peer = 'Aboba';
+CALL insert_p2p('Aboba', 'Sus', 'C3_s21_string', 'Start');
+
+SELECT *
+FROM transferred_points
+WHERE checking_peer = 'Sus'
+  AND checked_peer = 'Aboba';
 
 /* PART 4 */
-create trigger trg_xp
-before insert on xp
-for each row
-execute procedure fn_trg_xp();
+CREATE TRIGGER trg_xp
+    BEFORE INSERT
+    ON xp
+    FOR EACH ROW
+EXECUTE PROCEDURE fn_trg_xp();
 
-create or replace function fn_trg_xp()
-returns trigger as
-    $trg_xp$
-begin
-    if new.xp_amount > (select max_xp from tasks
-                                      join checks c on c.id = new.check_
-                                      where new.check_ = c.id
-                                      limit 1)
-    then
-        raise notice 'xp > max_xp';
-        return null;
-    end if;
-    if (select state_ from p2p where state_ = 'Success' and check_ = new.check_) is null
-    then
-        raise notice 'p2p stage not finished';
-        return null;
-    end if;
-    if (select state_ from verter where check_ = new.check_ limit 1) is not null
-           and (select state_ from verter where state_ = 'Success' and check_ = new.check_) is null
-    then
-        raise notice 'verter stage not finished';
-        return null;
-    end if;
-    return new;
-end;
-$trg_xp$ language 'plpgsql';
+CREATE OR REPLACE FUNCTION fn_trg_xp()
+    RETURNS TRIGGER AS
+$trg_xp$
+BEGIN
+    IF new.xp_amount > (SELECT max_xp
+                        FROM tasks
+                                 JOIN checks c ON c.id = new.check_
+                        WHERE new.check_ = c.id
+                        LIMIT 1)
+    THEN
+        RAISE NOTICE 'xp > max_xp';
+        RETURN NULL;
+    END IF;
+    IF (SELECT state_ FROM p2p WHERE state_ = 'Success' AND check_ = new.check_) IS NULL
+    THEN
+        RAISE NOTICE 'p2p stage NOT finished';
+        RETURN NULL;
+    END IF;
+    IF (SELECT state_ FROM verter WHERE check_ = new.check_ LIMIT 1) is NOT NULL
+        AND (SELECT state_ FROM verter WHERE state_ = 'Success' AND check_ = new.check_) IS NULL
+    THEN
+        RAISE NOTICE 'verter stage NOT finished';
+        RETURN NULL;
+    END IF;
+    RETURN new;
+END;
+$trg_xp$ LANGUAGE 'plpgsql';
 
-insert into xp (id, check_, xp_amount) values ((select max(id) from xp) + 1, 23, 1500);
+INSERT INTO xp (id, check_, xp_amount) VALUES ((SELECT MAX(id) FROM xp) + 1, 23, 1500);
 
-select (select state_ from p2p where state_ = 'Success' and check_ = 22) is not null;
-select state_ from verter where state_ = 'Success' and check_ = 22;
+SELECT (SELECT state_ FROM p2p WHERE state_ = 'Success' AND check_ = 22) IS NOT NULL;
+SELECT state_ FROM verter WHERE state_ = 'Success' AND check_ = 22;
